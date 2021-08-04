3f8a986a4446af2a53c9f72f470cc03ba986292d
Alexander Edwards <36854292+AlexEdwards11@users.noreply.github.com>
2021-08-04 16:46:23 -0500

    Added react router to app, and started homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import HomePage from "./Pages/Home";
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 
 export default App;
